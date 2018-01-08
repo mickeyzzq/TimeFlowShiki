44ed5ebb25a2f502ea8245c737aa4c4e805119fa
tomori_aki <tomori.aki2961@gmail.com>
2018-01-09 04:27:20 +0900

    Timelineがカラフルになるように更新

--- Assets/TimeFlowShiki/Editor/GUI/TimeFlowShikiGUISettings.cs
+++ Assets/TimeFlowShiki/Editor/GUI/TimeFlowShikiGUISettings.cs
@@ -53,19 +53,16 @@ namespace TimeFlowShiki
         public const string RESOURCE_BASEPATH = "Assets/TimeFlowShiki/Editor/GUI/Res/";
 
         public static List<Color> ResourceColorsSources = new List<Color> {
-            new Color(0.000f, 0.318f, 0.604f, 1.000f)
-            // ,
-            // new Color(0.620f, 0.137f, 0.494f, 1.000f),
-            // new Color(0.929f, 0.043f, 0.443f, 1.000f),
-            // new Color(0.929f, 0.102f, 0.231f, 1.000f),
-            // new Color(0.953f, 0.424f, 0.133f, 1.000f),
-            // new Color(1.000f, 0.812f, 0.012f, 1.000f),
-            // new Color(0.843f, 0.867f, 0.137f, 1.000f),
-            // new Color(0.306f, 0.690f, 0.286f, 1.000f),
-            // new Color(0.051f, 0.592f, 0.286f, 1.000f),
-            // new Color(0.000f, 0.620f, 0.620f, 1.000f),
-            // new Color(0.004f, 0.631f, 0.773f, 1.000f),
-            // new Color(0.039f, 0.553f, 0.804f, 1.000f),
+            new Color(0.000f, 0.318f, 0.604f, 1.000f),
+            new Color(0.620f, 0.137f, 0.494f, 1.000f),
+            new Color(0.929f, 0.043f, 0.443f, 1.000f),
+            new Color(0.929f, 0.102f, 0.231f, 1.000f),
+            new Color(0.953f, 0.424f, 0.133f, 1.000f),
+            new Color(0.306f, 0.690f, 0.286f, 1.000f),
+            new Color(0.051f, 0.592f, 0.286f, 1.000f),
+            new Color(0.000f, 0.620f, 0.620f, 1.000f),
+            new Color(0.004f, 0.631f, 0.773f, 1.000f),
+            new Color(0.039f, 0.553f, 0.804f, 1.000f)
         };
 
         public const string RESOURCE_TICK = RESOURCE_BASEPATH + "tick.png";
